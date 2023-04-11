76c3071f6c67fd55d4560047ce6cc84c7bff4d66
Mia Truong <tmanh1312@gmail.com>
2023-04-10 22:57:40 -0700

    6 and 7

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,4 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
+SELECT t.TrackId, t.Name, t.Composer 
+FROM tracks t 
+JOIN invoice_items ii ON t.TrackId = ii.TrackId;
--- question-7.sql
+++ question-7.sql
@@ -1,1 +1,6 @@
 -- Find tracks (id, name, and composer) that have never been purchased (that is, they aren't part of a line item in an invoice).
+-- Find tracks (id, name and composer) that are part of a line in an invoice.
+SELECT t.TrackId, t.Name, t.Composer, ii.TrackId 
+FROM tracks t 
+LEFT JOIN invoice_items ii ON t.TrackId = ii.TrackId
+WHERE ii.TrackId IS NULL;
