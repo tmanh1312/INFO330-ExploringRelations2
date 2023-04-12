501cdf64d6b6433653c53e977fdbc70d502bf872
Mia Truong <tmanh1312@gmail.com>
2023-04-11 18:07:11 -0700

    fix q5

--- question-5.sql
+++ question-5.sql
@@ -1,9 +1,9 @@
 -- What was the most purchased track of 2013?
-SELECT t.NAME
+SELECT t.*
 FROM tracks t 
 JOIN invoice_items ii ON t.TrackId = ii.TrackId
 JOIN invoices i ON ii.InvoiceId  = i.InvoiceId 
-WHERE STRFTIME('%Y', i.InvoiceDate) = '2013'
+WHERE i.InvoiceDate BETWEEN '2013-01-01' and '2013-12-31'
 GROUP BY t.TrackId
-ORDER BY SUM(ii.Quantity) DESC 
+ORDER BY COUNT(ii.Quantity) DESC 
 LIMIT 1;
